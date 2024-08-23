9f456b77560898e6af792bd43482cf7dee4fd51a
Ko-nifuramu <kotaro22235963@gmail.com>
2024-08-23 16:24:47 +0900

    fix: 104_maximum_depth_of_binary_tree

--- atari60/Graph_BFS_DFS/104_maximum_depth_of_binary_tree.py
+++ atari60/Graph_BFS_DFS/104_maximum_depth_of_binary_tree.py
@@ -41,3 +41,43 @@ class Solution:
             if node.right:
                 node_depth_queue.append((node.right, depth+1))
         return max_depth
+    
+
+
+'''
+step2
+'''
+
+#approach1: dfsで探索, こっちの方が重要
+#時間: 3:40
+#time: O(N)
+#space: O(N)木構造的に関数を実行しているのでスタックに貯まる
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        if not root.left and not root.right:
+            return 1
+            
+        max_depth = 0
+        max_depth = max(1+self.maxDepth(root.right), 1+self.maxDepth(root.left))
+        return max_depth
+
+#bfs
+#time: O(N)
+#space: O(N)枝分かれの分だけqueueに溜まっていく
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue_node_depth = deque([(root, 1)])
+        max_len = 0
+        while queue_node_depth:
+            node, depth = queue_node_depth.popleft()
+            if not node.left and not node.right:
+                max_len = max(max_len, depth)
+            if node.left:
+                queue_node_depth.append((node.left, depth+1))
+            if node.right:
+                queue_node_depth.append((node.right, depth+1))
+        return max_len
