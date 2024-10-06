d02e180d76af7e1912a6fb74a61bbf6977ffc4d4
Ko-nifuramu <kotaro22235963@gmail.com>
2024-10-06 15:38:22 +0900

    [add]: 198-step1

--- atari60/DynamicProgramming/139_word_break.py
+++ atari60/DynamicProgramming/139_word_break.py
@@ -1,52 +1,56 @@
 from typing import List
 
 
-'''
+"""
 Approach1: Dymnamic Programming
 
-time: O(N^3)
+time: O(N^2)
 space: O(N)
-'''
+"""
+
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
+
         if len(s) == 0:
             return True
 
         wordDict_set = set(wordDict)
-        
-        #segmentation_possible[i] means s[0:i] can be segmented into words in wordDict
+
+        # segmentation_possible[i] means s[0:i] can be segmented into words in wordDict
         segmentation_possible = [False] * len(s) + 1
         segmentation_possible[0] = True
-        
-        for end_idx in range(1, len(s)+1):
+
+        for end_idx in range(1, len(s) + 1):
             for start_idx in range(end_idx):
-                if segmentation_possible[start_idx] and s[start_idx:end_idx] in wordDict_set:
+                if (
+                    segmentation_possible[start_idx]
+                    and s[start_idx:end_idx] in wordDict_set
+                ):
                     segmentation_possible[end_idx] = True
-                    
-        return  segmentation_possible[-1]
-    
-    
-'''
+
+        return segmentation_possible[-1]
+
+
+"""
 Approach2: Rrcursive Algorithm
 
 time: O(N^2)
 space: O(N)
-'''
+"""
 
 from typing import List, Set
+
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         if not s:
             return True
-
         words_set = set(wordDict)
         memo = {}
 
         return self.word_break_helper(s, words_set, memo)
-    
+
     def word_break_helper(self, s: str, words_set: Set[str], memo: dict) -> bool:
         if s in memo:
             return memo[s]
@@ -65,3 +69,28 @@ class Solution:
 
         memo[s] = False
         return False
+
+
+#TLE
+#recursive
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        '''
+        applespaceapples
+        -> apple spaceapples or apples paceapples
+        wordDict [apple, space, apples] 
+        '''
+        can_word_break = False
+        def check_word_break(remaining_s):
+            nonlocal can_word_break
+            if can_word_break:
+                return
+            for word in wordDict:
+                if remaining_s == word:
+                    can_word_break = True
+                    return
+                if remaining_s[:len(word)] == word:
+                    check_word_break(remaining_s[len(word):])
+        
+        check_word_break(s)
+        return can_word_break
