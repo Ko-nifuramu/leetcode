7205b6797bb8f76c69d4f455bed67c55de15b493
Ko-nifuramu <kotaro22235963@gmail.com>
2024-12-01 19:09:24 +0900

    [feat]: 35-step3

--- arai60/BinarySearch/35_search_insert_position.py
+++ arai60/BinarySearch/35_search_insert_position.py
@@ -47,3 +47,25 @@ class Solution:
             else:
                 right = mid
         return left
+
+
+
+"""
+step3
+"""
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            middle = (left+right)//2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+
+        return left
